28a13a1374ccef2b8518c9b6b85d28705d9a1c3a
Andreas Lidholm <lidholm.andreas@gmail.com>
2015-01-20 12:55:54 +0100

    More items

--- controllers.js
+++ controllers.js
@@ -2,16 +2,23 @@ var phonecatApp = angular.module('phonecatApp', []);
 
 phonecatApp.controller('PhoneListCtrl', function ($scope) {
   $scope.items = [
-    {'name': 'Floor1', 'top': 0, 'left': 0, 'height': 382, 'width': 336, 'color': '#ddd', 'zIndex': 1},
-	{'name': 'Floor2', 'top': 44, 'left': 336, 'height': 338, 'width': 110, 'color': '#ddd', 'zIndex': 1},
-	{'name': 'Door1', 'top': 200, 'left': 444, 'height': 80, 'width': 4, 'color': '#fff', 'zIndex': 2},
-	{'name': 'Door2', 'top': 380, 'left': 300, 'height': 4, 'width': 80, 'color': '#fff', 'zIndex': 2},
-	{'name': 'Door3', 'top': -2, 'left': 236, 'height': 4, 'width': 80, 'color': '#fff', 'zIndex': 2},
+    {'name': 'Floor1', 'top': 0, 'left': 0, 'height': 382, 'width': 336, 'color': '#ddd', 'zIndex': 1, 'group':'Floor'},
+	{'name': 'Floor2', 'top': 44, 'left': 336, 'height': 338, 'width': 110, 'color': '#ddd', 'zIndex': 1, 'group':'Floor'},
+	{'name': 'DoorSovrum', 'top': 154, 'left': 444, 'height': 80, 'width': 4, 'color': '#fff', 'zIndex': 2},
+	{'name': 'DoorBalkong', 'top': 380, 'left': 300, 'height': 4, 'width': 80, 'color': '#fff', 'zIndex': 2},
+	{'name': 'DoorHall', 'top': -2, 'left': 236, 'height': 4, 'width': 80, 'color': '#fff', 'zIndex': 2},
 	{'name': 'Window', 'top': 380, 'left': 40, 'height': 4, 'width': 100, 'color': '#fff', 'zIndex': 2},
 	
 	
-    {'name': 'Soffa', 'top': 320, 'left': 0, 'height': 60, 'width': 220, 'color': '#B97A57', 'zIndex': 2},
-    {'name': 'Tv-bänk', 'top': 0, 'left': 50, 'height': 50, 'width': 160, 'color': '#222', 'zIndex': 2},
+    {'name': 'Soffa',     'top': 300, 'left': 0,   'height': 80, 'width': 220, 'color': '#c86', 'zIndex': 2},
+    {'name': 'Soffbord1', 'top': 220, 'left': 50,  'height': 55, 'width': 55,  'color': '#222', 'zIndex': 2},
+    {'name': 'Soffbord2', 'top': 220, 'left': 110, 'height': 55, 'width': 55,  'color': '#222', 'zIndex': 2},
+    {'name': 'Tv-bänk',   'top': 0,   'left': 40,  'height': 50, 'width': 160, 'color': '#222', 'zIndex': 2},
+    {'name': 'Spritskåp', 'top': 0,   'left': 0,   'height': 40, 'width': 40,  'color': '#fff', 'zIndex': 2},
+    {'name': 'Spritskåp', 'top': 44,  'left': 336, 'height': 30, 'width': 40,  'color': '#222', 'zIndex': 2},
+    {'name': 'Spritskåp', 'top': 44,  'left': 376, 'height': 30, 'width': 40,  'color': '#222', 'zIndex': 2},
+    {'name': 'Spritskåp', 'top': 74,  'left': 416, 'height': 80, 'width': 30,  'color': '#222', 'zIndex': 2},
+    {'name': 'Gungstol',  'top': 300, 'left': 370, 'height': 60, 'width': 60,  'color': '#aca', 'zIndex': 2},
     
   ];
 });
