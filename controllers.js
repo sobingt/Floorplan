32cbbbb407e735cb45be906602deab77ca0f0ff5
Andreas Lidholm <lidholm.andreas@gmail.com>
2015-01-20 21:33:18 +0100

    Create offset and multiplier variables to better fit window sizes

--- controllers.js
+++ controllers.js
@@ -2,12 +2,12 @@ var phonecatApp = angular.module('phonecatApp', []);
 
 phonecatApp.controller('PhoneListCtrl', function ($scope) {
   $scope.items = [
-    {'name': 'Golv1', 'top': 0, 'left': 0, 'height': 382, 'width': 336, 'color': '#ddd', 'zIndex': 1, 'group':'Golv'},
-	{'name': 'Golv2', 'top': 44, 'left': 336, 'height': 338, 'width': 110, 'color': '#ddd', 'zIndex': 1, 'group':'Golv'},
-	{'name': 'Dörr Sovrum', 'top': 154, 'left': 444, 'height': 80, 'width': 4, 'color': '#fff', 'zIndex': 2},
-	{'name': 'Dörr Balkong', 'top': 380, 'left': 300, 'height': 4, 'width': 80, 'color': '#fff', 'zIndex': 2},
-	{'name': 'Dörr Hall', 'top': -2, 'left': 236, 'height': 4, 'width': 80, 'color': '#fff', 'zIndex': 2},
-	{'name': 'Fönster', 'top': 380, 'left': 40, 'height': 4, 'width': 100, 'color': '#fff', 'zIndex': 2},
+    {'name': 'Golv1',          'top': 0,   'left': 0,   'height': 382, 'width': 336, 'color': '#ddd', 'zIndex': 1, 'group':'Golv'},
+	{'name': 'Golv2',          'top': 44,  'left': 336, 'height': 338, 'width': 110, 'color': '#ddd', 'zIndex': 1, 'group':'Golv'},
+	{'name': 'Dörr Sovrum',    'top': 154, 'left': 444, 'height': 80,  'width': 4,   'color': '#fff', 'zIndex': 2},
+	{'name': 'Dörr Balkong',   'top': 380, 'left': 300, 'height': 4,   'width': 80,  'color': '#fff', 'zIndex': 2},
+	{'name': 'Dörr Hall',      'top': -2,  'left': 236, 'height': 4,   'width': 80,  'color': '#fff', 'zIndex': 2},
+	{'name': 'Fönster',        'top': 380, 'left': 40,  'height': 4,   'width': 100, 'color': '#fff', 'zIndex': 2},
 	
 	
     {'name': 'Soffa',     'top': 300, 'left': 0,   'height': 80, 'width': 220, 'color': '#c86', 'zIndex': 2},
@@ -21,6 +21,10 @@ phonecatApp.controller('PhoneListCtrl', function ($scope) {
     {'name': 'Gungstol',  'top': 300, 'left': 370, 'height': 60, 'width': 60,  'color': '#aca', 'zIndex': 2},
     
   ];
+  
+  $scope.multiplier = 1.5;
+  $scope.offsetTop = 5;
+  $scope.offsetLeft = 5;
   
   $scope.selectItem = function(item) {
     $scope.selectedItem = item;
