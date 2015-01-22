5981cd77cde921ff0b30c135265a57fdafbe3ea5
Andreas Lidholm <lidholm.andreas@gmail.com>
2015-01-22 23:35:36 +0100

    Convert some of the out commented items to use parts

--- controllers.js
+++ controllers.js
@@ -13,20 +13,68 @@ phonecatApp.controller('PhoneListCtrl', function ($scope, $log) {
 	  'visible':true,
 	  'parts' : [
         { 'top': 162, 'left': 444, 'height': 96,  'width': 4,   'color': '#fff', 'zIndex': 200, 'border': 0,},
-	  ]}
-	/*{'name': 'Dörr Balkong',   'top': 380, 'left': 235, 'height': 4,   'width': 128,  'color': '#fff', 'zIndex': 200, 'border': 0, 'visible':true ,},
-	{'name': 'Dörr Hall',      'top': -2,  'left': 232, 'height': 4,   'width': 95,  'color': '#fff', 'zIndex': 200, 'border': 0, 'visible':true ,},
-	{'name': 'Fönster',        'top': 380, 'left': 58,  'height': 4,   'width': 126, 'color': '#fff', 'zIndex': 200, 'border': 0, 'visible':true ,},
+	  ]},
+	{ 'name': 'Dörr Balkong', 
+	  'visible':true,
+	  'parts' : [
+        {'top': 380, 'left': 235, 'height': 4,   'width': 128,  'color': '#fff', 'zIndex': 200, 'border': 0,},
+	  ]},
+	{ 'name': 'Dörr Hall', 
+	  'visible':true,
+	  'parts' : [
+        {'top': -2,  'left': 232, 'height': 4,   'width': 95,  'color': '#fff', 'zIndex': 200, 'border': 0,},
+	  ]},
+	{ 'name': 'Fönster',
+	  'visible':true,
+	  'parts' : [
+        {'top': 380, 'left': 58,  'height': 4,   'width': 126, 'color': '#fff', 'zIndex': 200, 'border': 0,},
+	  ]},
 	
-    {'name': 'Soffa',     'top': 267, 'left': 10,   'height': 90, 'width': 220, 'color': '#c86', 'zIndex': 2, 'border': 0, 'visible':true, },
-    {'name': 'Soffbord1', 'top': 192, 'left': 50,  'height': 55, 'width': 55,  'color': '#222', 'zIndex': 2, 'border': 0, 'visible':true ,},
-    {'name': 'Soffbord2', 'top': 192, 'left': 110, 'height': 55, 'width': 55,  'color': '#222', 'zIndex': 2, 'border': 0, 'visible':true ,},
-    {'name': 'Tv-bänk',   'top': 11,   'left': 35,  'height': 50, 'width': 191, 'color': '#222', 'zIndex': 2, 'border': 0, 'visible':true ,},
-    {'name': 'Spritskåp', 'top': 0,   'left': 0,   'height': 30, 'width': 30,  'color': '#fff', 'zIndex': 2, 'border': 0, 'visible':true ,},
-    {'name': 'Hylla1', 'top': 44,  'left': 336, 'height': 30, 'width': 40,  'color': '#222', 'zIndex': 2, 'border': 0, 'visible':true ,},
-    {'name': 'Hylla2', 'top': 44,  'left': 376, 'height': 30, 'width': 40,  'color': '#222', 'zIndex': 2, 'border': 0, 'visible':true ,},
-    {'name': 'Hylla3', 'top': 84,  'left': 416, 'height': 80, 'width': 30,  'color': '#222', 'zIndex': 2, 'border': 0, 'visible':true ,},
-    {'name': 'Gungstol',  'top': 300, 'left': 370, 'height': 60, 'width': 67,  'color': '#aca', 'zIndex': 2, 'border': 0, 'visible':true ,},*/
+    { 'name': 'Soffa',
+	  'visible':true,
+	  'parts' : [
+        {'top': 267, 'left': 10,   'height': 90, 'width': 220, 'color': '#c86', 'zIndex': 2, 'border': 0,},
+	  ]},
+    { 'name': 'Soffbord1', 
+	  'visible':true,
+	  'parts' : [
+        { 'top': 192, 'left': 50,  'height': 55, 'width': 55,  'color': '#222', 'zIndex': 2, 'border': 0,},
+	  ]},
+    { 'name':'Soffbord2', 
+	  'visible':true,
+	  'parts' : [
+        { 'top': 192, 'left': 110, 'height': 55, 'width': 55,  'color': '#222', 'zIndex': 2, 'border': 0,},
+	  ]},
+    { 'name':'Tv-bänk',   
+	  'visible':true,
+	  'parts' : [
+        { 'top': 11,   'left': 35,  'height': 50, 'width': 191, 'color': '#222', 'zIndex': 2, 'border': 0,},
+	  ]},
+    { 'name':'Spritskåp', 
+	  'visible':true,
+	  'parts' : [
+        { 'top': 0,   'left': 0,   'height': 30, 'width': 30,  'color': '#fff', 'zIndex': 2, 'border': 0,},
+	  ]},
+    { 'name':'Hylla1', 
+	  'visible':true,
+	  'parts' : [
+        { 'top': 44,  'left': 336, 'height': 30, 'width': 40,  'color': '#222', 'zIndex': 2, 'border': 0,},
+	  ]},
+    { 'name':'Hylla2', 
+	  'visible':true,
+	  'parts' : [
+        { 'top': 44,  'left': 376, 'height': 30, 'width': 40,  'color': '#222', 'zIndex': 2, 'border': 0,},
+	  ]},
+    { 'name':'Hylla3', 
+	  'visible':true,
+	  'parts' : [
+        { 'top': 84,  'left': 416, 'height': 80, 'width': 30,  'color': '#222', 'zIndex': 2, 'border': 0,},
+	  ]},
+    { 'name': 'Gungstol',  
+	  'visible':true,
+	  'parts' : [
+        { 'top': 300, 'left': 370, 'height': 60, 'width': 67,  'color': '#aca', 'zIndex': 2, 'border': 0,},
+	  ]},
     ]},
 	
   {'name':'Bedroom', 'items' : [
@@ -41,14 +89,14 @@ phonecatApp.controller('PhoneListCtrl', function ($scope, $log) {
 	  'parts' : [
 	    { 'top': 132, 'left': -2, 'height': 96,  'width': 4,   'color': '#fff', 'zIndex': 200, 'border': 0,},
 	  ]}
-	/*{'name': 'Fönster',          'top': 380, 'left': 58,  'height': 4,   'width': 126, 'color': '#fff', 'zIndex': 200, 'border': 0, 'visible':true ,},
+	/*{'name': 'Fönster',          'top': 380, 'left': 58,  'height': 4,   'width': 126, 'color': '#fff', 'zIndex': 200, 'border': 0,},
 	
-    {'name': 'Säng',     'top': 100,  'left': 140,   'height': 180, 'width': 200, 'color': '#fff', 'zIndex': 2, 'border': 0, 'visible':true, },
-    {'name': 'Headboard', 'top': 70,  'left': 339,  'height': 240, 'width': 8,  'color': '#222', 'zIndex': 2, 'border': 0, 'visible':true ,},
-    {'name': 'Garderob1', 'top': 0, 'left': 0, 'height': 60, 'width': 60,  'color': '#fff', 'zIndex': 2, 'border': 0, 'visible':true ,},
-    {'name': 'Garderob2', 'top': 61,  'left': 0,  'height': 60, 'width': 60, 'color': '#fff', 'zIndex': 2, 'border': 0, 'visible':true ,},
-    {'name': 'Garderob3', 'top': 244,   'left': 0,   'height': 60, 'width': 60,  'color': '#fff', 'zIndex': 2, 'border': 0, 'visible':true ,},
-    {'name': 'Garderob4', 'top': 305,  'left': 0, 'height': 60, 'width': 60,  'color': '#fff', 'zIndex': 2, 'border': 0, 'visible':true ,},*/
+    {'name': 'Säng',     'top': 100,  'left': 140,   'height': 180, 'width': 200, 'color': '#fff', 'zIndex': 2, 'border': 0,},
+    {'name': 'Headboard', 'top': 70,  'left': 339,  'height': 240, 'width': 8,  'color': '#222', 'zIndex': 2, 'border': 0,},
+    {'name': 'Garderob1', 'top': 0, 'left': 0, 'height': 60, 'width': 60,  'color': '#fff', 'zIndex': 2, 'border': 0,},
+    {'name': 'Garderob2', 'top': 61,  'left': 0,  'height': 60, 'width': 60, 'color': '#fff', 'zIndex': 2, 'border': 0,},
+    {'name': 'Garderob3', 'top': 244,   'left': 0,   'height': 60, 'width': 60,  'color': '#fff', 'zIndex': 2, 'border': 0,},
+    {'name': 'Garderob4', 'top': 305,  'left': 0, 'height': 60, 'width': 60,  'color': '#fff', 'zIndex': 2, 'border': 0,},*/
     ]},
 	
 	
